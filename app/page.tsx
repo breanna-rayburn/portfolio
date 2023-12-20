475fbc7b0fe5c4aea0ef0990741ba8eb725f5d18
breanna-rayburn <breanna.rayburn00@gmail.com>
2023-12-19 23:08:25 -0800

    added nvmrc

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <div>
         <h1>hello world this is a test</h1>
       </div>
